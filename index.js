cac9371af09a77a170041e913cf295eab7545863
kay-bankz-cfu <abdulrahmanbadmus240@gmail.com>
2023-10-04 03:30:59 +0100

    Calculator

---
+++ index.js
@@ -0,0 +1,23 @@
+// STRING
+
+let a = "Stella"
+document.getElementById(a).innerHTML = a
+console.log(a)
+
+// NUMBERS
+
+let x = 5
+console.log(x)
+
+var age = 10
+console.log(age)
+
+// BOOLEAN
+
+let ismale = true
+console.log(!ismale)
+
+// ARREY
+
+let courses = ["Elect Elect", "Medicine", "Civil Engineer"];
+console.log(courses)
