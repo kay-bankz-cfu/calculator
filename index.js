8d438dfb4349927b9f1147fb4e711957b78a53eb
kay-bankz-cfu <abdulrahmanbadmus240@gmail.com>
2023-10-05 05:40:06 +0100

    Tic Tac

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // STRING
 
 let a = "Stella"
-document.getElementById(a).innerHTML = a
 console.log(a)
 
 // NUMBERS
@@ -12,7 +11,7 @@ console.log(x)
 var age = 10
 console.log(age)
 
-// BOOLEAN
+// BOOLEAN          jhccsuy8dcgdyg8ygdchudic8digcygysudhcu
 
 let ismale = true
 console.log(!ismale)
@@ -21,3 +20,54 @@ console.log(!ismale)
 
 let courses = ["Elect Elect", "Medicine", "Civil Engineer"];
 console.log(courses)
+
+
+
+
+
+
+
+
+function henrry(){
+    console.log("Henrry don cash out")
+}
+henrry()
+
+function Sumoftwonumbers(henrry, exchange) {
+    console.log("sumofhenrry and echange", henrry + exchange)
+}
+Sumoftwonumbers(100, 100)
+
+
+
+
+
+
+// function exchange()
+// console.log(exchange)
+// exchange()  
+
+
+
+
+const Display = document.querySelector('#apple')
+
+Display.addEventListener('click', show)
+
+
+function show(){
+    Display.innerHTML="Lawal, You are welcome to the hub"
+    alert('Welcome to our page')
+}
+
+
+
+
+// const cat = document.getElementById("ball")
+
+// .addEventListener("mouseover",play)
+
+// function play(){
+//     Display.innerHTML="mhide"
+
+// }
---
+++ xo/index.js
@@ -0,0 +1,90 @@
+// X
+
+const o = document.getElementById("zebra1")
+o.addEventListener("click", key)
+
+function key(){
+    o.innerHTML= 'X'
+}
+
+
+
+const p = document.getElementById("zebra2")
+p.addEventListener("click", keyy)
+
+function keyy(){
+    p.innerHTML= 'X'
+}
+
+
+
+const q = document.getElementById("zebra3")
+q.addEventListener("click", keyyy)
+
+function keyyy(){
+    q.innerHTML= 'X'
+}
+
+
+// O
+
+const a = document.getElementById("zebra11")
+a.addEventListener("click", mouse)
+
+function mouse(){
+    a.innerHTML= 'O'
+}
+
+
+const b = document.getElementById("zebra22")
+b.addEventListener("click", mousee)
+
+function mousee(){
+    b.innerHTML= 'O'
+}
+
+
+const c = document.getElementById("zebra33")
+c.addEventListener("click", mouseee)
+
+function mouseee(){
+    c.innerHTML= 'O'
+}
+
+
+// X
+
+const m = document.getElementById("zebra111")
+m.addEventListener("click", phone)
+
+function phone(){
+    m.innerHTML= 'X'
+}
+
+
+const n = document.getElementById("zebra222")
+n.addEventListener("click", phonee)
+
+function phonee(){
+    n.innerHTML= 'X'
+}
+
+
+const g = document.getElementById("zebra333")
+g.addEventListener("click", phoneee)
+
+function phoneee(){
+    g.innerHTML= 'X'
+}
+
+
+// PLAY
+
+// const game = document.getElementsByClassName("cat")
+
+// game.addEventListener("mouseover", start)
+
+// function start(){
+//     game.innerHTML="Enjoy The Game"
+//     alert=("Play & Win 🎗")
+// }
